4e37fa6312b8f1ede8c1f5e0418065c69fadd229
Tuyen <1612792@student.hcmus.edu.vn>
2019-10-23 15:35:01 +0700

    Add router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
+import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import GameCaro from './containers/GameCaro/GameCaro';
-
 import './App.css';
 
 class App extends Component {
@@ -13,6 +13,11 @@ class App extends Component {
   render() {
     return (
       <Layout>
+        <Switch>
+          <Route path="/login" Component=""/>
+          <Route path="/register" Component=""/>
+          <Route path="/" Component={GameCaro}/>
+        </Switch>
         <GameCaro />
       </Layout>
     )
