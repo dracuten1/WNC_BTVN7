27c9d19fe97720ae867cc70a46f3da6286617785
Tuyen <1612792@student.hcmus.edu.vn>
2019-11-06 20:33:22 +0700

    desabled https

--- src/axios-backend.js
+++ src/axios-backend.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://ec2-13-250-157-230.ap-southeast-1.compute.amazonaws.com:8626'
+    baseURL: 'https://ec2-13-250-157-230.ap-southeast-1.compute.amazonaws.com:443'
     //baseURL: 'http://localhost:8626'
 });
 
