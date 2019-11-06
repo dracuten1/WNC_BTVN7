542672aeb3518534786554695ec24d0efab62898
Tuyen <1612792@student.hcmus.edu.vn>
2019-11-06 20:18:12 +0700

    Updates

---
+++ precache-manifest.fd33cc3f5af639833f58ef6d7e7924ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6026ff1a61f80beaef6e190e9275100d",
+    "url": "/WNC_BTVN7/index.html"
+  },
+  {
+    "revision": "147b73a8a514474c3a24",
+    "url": "/WNC_BTVN7/static/css/main.7a77b620.chunk.css"
+  },
+  {
+    "revision": "e6c8fb960ffb5eb6ae34",
+    "url": "/WNC_BTVN7/static/js/2.29a9cd91.chunk.js"
+  },
+  {
+    "revision": "147b73a8a514474c3a24",
+    "url": "/WNC_BTVN7/static/js/main.a83bed09.chunk.js"
+  },
+  {
+    "revision": "40384fcabea6277338ed",
+    "url": "/WNC_BTVN7/static/js/runtime~main.a2e783da.js"
+  }
+]);
