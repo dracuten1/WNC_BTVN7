090cf02d21ff455abeb0b958e1a66f62633da1f5
Tuyen <1612792@student.hcmus.edu.vn>
2019-11-06 20:27:49 +0700

    change server add

--- src/axios-backend.js
+++ src/axios-backend.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    //baseURL: 'http://ec2-13-250-157-230.ap-southeast-1.compute.amazonaws.com:8626'
-    baseURL: 'http://localhost:8626'
+    baseURL: 'https://ec2-13-250-157-230.ap-southeast-1.compute.amazonaws.com:8626'
+    //baseURL: 'http://localhost:8626'
 });
 
 export default instance;
